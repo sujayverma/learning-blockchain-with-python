20d12c8b4df0e5eca592510bf7ae7073183ef580
ext_s.barma <sujay.barma@indusnet.co.in>
2022-10-22 16:29:14 +0530

    installed ganache-cli built contract and interacted with deployed contract using ganache-cli

--- web3_py_simple_storage/deploy.py
+++ web3_py_simple_storage/deploy.py
@@ -38,9 +38,9 @@ abi = compile_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
 # print(compile_sol)
 # exit()
 # For connecting to Ganache.
-w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:7545"))
+w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
 chain_id = 1337
-my_address = "0xaC6782447537E8c053C73E958298D0D2C864c5aC"
+my_address = "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1"
 load_dotenv()
 private_key = os.getenv("PRIVATE_KEY")
 
@@ -60,3 +60,21 @@ signed_txn = w3.eth.account.sign_transaction(
 # Send this signed tranaction.
 tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
 tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+# Working With Contract.
+# We need Contract Address.
+# And Contract ABI
+# Interacting with contract.
+simple_storage = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
+# Now to interact we normally use 2 functions.
+# 1) Call -> Simulate making the call and getting a return value. This is usaully used for view functions.
+# 2) Transact -> Actually make a state change. This is usaully used for making state changes like storing a values.
+print(simple_storage.functions.retrive().call())
+
+store_transaction = simple_storage.functions.store(10).buildTransaction({
+    "gasPrice": w3.eth.gas_price, "chainId": chain_id, "from": my_address, "nonce": nonce + 1
+})
+signed_store_tx = w3.eth.account.sign_transaction(
+    store_transaction, private_key=private_key)
+store_txn_hash = w3.eth.send_raw_transaction(signed_store_tx.rawTransaction)
+store_txn_receipt = w3.eth.wait_for_transaction_receipt(store_txn_hash)
+print(simple_storage.functions.retrive().call())
