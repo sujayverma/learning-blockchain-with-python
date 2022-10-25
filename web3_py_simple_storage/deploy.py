aa0010cbc7471e946bab9125f8338b3dfd052852
ext_s.barma <sujay.barma@indusnet.co.in>
2022-10-25 16:27:53 +0530

    Added Infura for testing in Live Gorli Testnet

--- web3_py_simple_storage/deploy.py
+++ web3_py_simple_storage/deploy.py
@@ -38,12 +38,17 @@ abi = compile_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
 # print(compile_sol)
 # exit()
 # For connecting to Ganache.
-w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
-chain_id = 1337
-my_address = "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1"
+# w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
+w3 = Web3(Web3.HTTPProvider(
+    "https://goerli.infura.io/v3/93004f82f2e047f790afea769e7ee86f"))
+# chain_id = 1337
+chain_id = 5
+# my_address = "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1"
+my_address = "0x9a2C3A404B6e10d477d310854d20bCd9970acAC7"
 load_dotenv()
 private_key = os.getenv("PRIVATE_KEY")
 
+print("Build a Contract...")
 # Create the Contract in python.
 SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
 # Get latest transaction.
@@ -55,11 +60,12 @@ transaction = SimpleStorage.constructor().build_transaction(
     {"gasPrice": w3.eth.gas_price, "chainId": chain_id, "from": my_address, "nonce": nonce})
 signed_txn = w3.eth.account.sign_transaction(
     transaction, private_key=private_key)
-# print(signed_txn)
+print("Sign a Contract")
 
 # Send this signed tranaction.
 tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
 tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+print("Send and Deploy a Contract")
 # Working With Contract.
 # We need Contract Address.
 # And Contract ABI
@@ -68,6 +74,7 @@ simple_storage = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
 # Now to interact we normally use 2 functions.
 # 1) Call -> Simulate making the call and getting a return value. This is usaully used for view functions.
 # 2) Transact -> Actually make a state change. This is usaully used for making state changes like storing a values.
+print("Interact With Contract")
 print(simple_storage.functions.retrive().call())
 
 store_transaction = simple_storage.functions.store(10).buildTransaction({
@@ -77,4 +84,5 @@ signed_store_tx = w3.eth.account.sign_transaction(
     store_transaction, private_key=private_key)
 store_txn_hash = w3.eth.send_raw_transaction(signed_store_tx.rawTransaction)
 store_txn_receipt = w3.eth.wait_for_transaction_receipt(store_txn_hash)
+print("Make state change to contract.")
 print(simple_storage.functions.retrive().call())
